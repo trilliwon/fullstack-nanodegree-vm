a0bdcae46cbb8ecbed7c700152eef0d671b54654
kyle.jo <kyle.jo@kakaocorp.com>
2019-04-22 22:03:33 +0900

    update

--- vagrant/restaurantmenu/finalproject.py
+++ vagrant/restaurantmenu/finalproject.py
@@ -11,6 +11,12 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[item.serialize for item in items])
+
 # Show all restaurants
 @app.route('/')
 @app.route('/restaurant/')
